7c124e279eab839e3a90a51d10534b0b7211075a
SmartOmaru <omar2004j@gmail.com>
2025-08-25 00:18:20 +0300

    Add Autobot and Decpticon subclasses

--- main.cpp
+++ main.cpp
@@ -1,30 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-
-class Character {
-
-private:
-    int health = 100;
-    string name;
-    bool current_turn;
-
-public:
-    Character(string name, bool turn = false, int health = 100) 
-    : name(name), current_turn(turn), health(health) {}
-
-    // Getter methods
-    int current_health() const { return health; }
-    string get_name() const { return name; }
-    bool is_turn() const { return current_turn; }
-    bool is_alive() const { return health > 0; }
-
-    // Method to take damage and decrease health accordingly
-    void take_damage(int damage_taken = 0) { 
-        health -= damage_taken;
-        if (health < 0) health = 0;
-    }
-};
 
 class Weapons {
 
@@ -46,8 +23,92 @@ public:
     
 
     // Method to see if the attack hits the target
-    bool attempt_hit(const Weapons& weapon) const {
+    bool attempt_hit(int chance_increase = 0) const {
         // Simple hit chance calculation (could be improved)
-        return (rand() % 100) < (weapon.get_accuracy() * 100);
+        return (rand() % 100) < (accuracy * (100 + chance_increase));
+    }
+};
+
+class Character {
+
+private:
+    int health = 100;
+    string name;
+    bool current_turn;
+    vector<Weapons> arsenal;
+
+protected:
+    vector<Weapons>& get_arsenal() { return arsenal; }
+    const vector<Weapons>& get_arsenal() const { return arsenal; }
+
+public:
+    Character(string name, bool turn = false, int health = 100, vector<Weapons> arsenal)
+    : name(name), current_turn(turn), health(health), arsenal(arsenal) {}
+
+    // Getter methods
+    int get_health() const { return health; }
+    string get_name() const { return name; }
+    bool is_turn() const { return current_turn; }
+    bool is_alive() const { return health > 0; }
+
+    // Method to take damage and decrease health accordingly
+    void take_damage(int damage_taken = 0) { 
+        health -= damage_taken;
+        if (health < 0) health = 0;
+    }
+
+    // Virtual method for attacking depending on character (polymorphism)
+    virtual void attack(Character& target) = 0;
+};
+
+class Autobot : public Character {
+
+public:
+    Autobot(string name, bool turn = false, int health = 100, vector<Weapons> arsenal)
+    : Character(name, turn, health, arsenal) {}
+
+    void attack(Character& target) override {
+        int chance_increase = 0;
+        Weapons& weapon = get_arsenal()[rand() % get_arsenal().size()];
+
+        // Roll for a special mode that increases chance to hit (chance is inversely proportional with health)
+        if (rand() % 100 < (100.0 / get_health())) {
+            cout << get_name() << " is in last stand mode!" << endl;
+            chance_increase = 20;  // Increase chance by 20%
+        }
+
+        if (weapon.attempt_hit(chance_increase)) {
+            target.take_damage(weapon.get_damage());
+            cout << get_name() << " attacks " << target.get_name() << " with " << weapon.get_name()
+                 << " for " << weapon.get_damage() << " damage!" << endl;
+        } else {
+            cout << get_name() << " missed the attack on " << target.get_name() << "!" << endl;
+        }
+    }
+};
+
+class Decepticon : public Character {
+
+public:
+    Decepticon(string name, bool turn = false, int health = 100, vector<Weapons> arsenal)
+    : Character(name, turn, health, arsenal) {}
+
+    void attack(Character& target) override {
+        int damage_increase = 0;
+        Weapons& weapon = get_arsenal()[rand() % get_arsenal().size()];
+
+        // Roll for a special mode that increases damage (chance is inversely proportional with health)
+        if (rand() % 100 < (100.0 / get_health())) {
+            cout << get_name() << " is in all out mode!" << endl;
+            damage_increase = static_cast<int>(weapon.get_damage() * 0.2);  // Increase damage by 20%
+        }
+
+        if (weapon.attempt_hit()) {
+            target.take_damage(weapon.get_damage() + damage_increase);
+            cout << get_name() << " attacks " << target.get_name() << " with " << weapon.get_name()
+                 << " for " << weapon.get_damage() + damage_increase << " damage!" << endl;
+        } else {
+            cout << get_name() << " missed the attack on " << target.get_name() << "!" << endl;
+        }
     }
 };
