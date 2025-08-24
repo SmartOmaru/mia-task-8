4b931d030381bd073ae5f86a15a34b74a4699de6
SmartOmaru <omar2004j@gmail.com>
2025-08-24 23:03:00 +0300

    Add Weapons class and .gitignore

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
+#include <iostream>
 #include <string>
 using namespace std;
 
 class Character {
+
 private:
     int health = 100;
     string name;
     bool current_turn;
+
 public:
     Character(string name, bool turn = false, int health = 100) 
     : name(name), current_turn(turn), health(health) {}
 
-    // Accessor methods
+    // Getter methods
     int current_health() const { return health; }
     string get_name() const { return name; }
     bool is_turn() const { return current_turn; }
@@ -20,5 +23,31 @@ public:
     void take_damage(int damage_taken = 0) { 
         health -= damage_taken;
         if (health < 0) health = 0;
+    }
+};
+
+class Weapons {
+
+private:
+    string name;
+    int damage;
+    float accuracy;
+    string description;
+
+public:
+    Weapons(string name, int damage, float accuracy, string description)
+    : name(name), damage(damage), accuracy(accuracy), description(description) {}
+
+    // Getter methods
+    string get_name() const { return name; }
+    int get_damage() const { return damage; }
+    float get_accuracy() const { return accuracy; }
+    string get_description() const { return description; }
+    
+
+    // Method to see if the attack hits the target
+    bool attempt_hit(const Weapons& weapon) const {
+        // Simple hit chance calculation (could be improved)
+        return (rand() % 100) < (weapon.get_accuracy() * 100);
     }
 };
